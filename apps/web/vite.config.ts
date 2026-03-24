ba2af67c04272fffd0b3b839612ff35c32ef5b79
Neil Fisher <neil3524@gmail.com>
2026-03-24 14:24:06 -0400

    update deps

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,10 +2,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite-plus";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [tailwindcss(), tanstackStart(), viteReact()],
   server: {
     port: 3001,
   },
